a1748df80b6330536ace4ae9ea479a39cce4aa6f
mariagwyn <maria.mcdowell@acquia.com>
2015-12-22 14:16:43 -0800

    Deleting. Obsolete. See https://github.com/acquia/drupal-core-patches instead.

--- scripts/acquiadrupal-false.sh
+++
@@ -1,8 +0,0 @@
-# Script sets IS ACQUIA DRUPAL to FALSE
-#
-# must: chmod +x ~/rmv_acquiadrupal.sh (makes executable)
-#
-# - @mariagwyn
-sed -i '' '/IS_ACQUIA_DRUPAL/s/TRUE/FALSE/g' profiles/acquia/modules/acquia_connector/acquia_agent/acquia_agent_drupal_version.inc
-
-echo "IS_ACQUIA_DRUPAL is now set to FALSE"
